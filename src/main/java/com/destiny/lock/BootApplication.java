32b0832e0ca65a448548b37c0919e4b58b4f9cdb
tianlong.ztl <payne.zhang@aihuishou.com>
2018-08-01 23:37:06 +0800

    jedis

--- src/main/java/com/destiny/lock/BootApplication.java
+++ src/main/java/com/destiny/lock/BootApplication.java
@@ -2,11 +2,13 @@ package com.destiny.lock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by zhangtianlong on 18/7/28.
  */
 @SpringBootApplication
+@ImportResource(value = {"classpath:application-bean.xml"})
 public class BootApplication {
 
     public static void main(String[] args) {
